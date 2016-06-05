574901697ccfb87afae48b5194088a1cb28c5968
ndhuanhuan <ndhuanhuan@gmail.com>
2016-06-06 06:11:39 +0800

    [146. LRU Cache][Compile Error]committed by LeetCode Extension

--- 146-LRU-Cache/146. LRU Cache.cpp
+++ 146-LRU-Cache/146. LRU Cache.cpp
@@ -1,91 +1,104 @@
 class LRUCache{
-    
-    struct Node {
+public:
+
+     struct Node {
         int val;
         int key;
         Node *next;
         Node *prev;
         Node(int k, int v):key(k),val(v) {}
     };
-    
+
     int maxSize;
     Node* head;
     Node* tail;
     unordered_map<int,Node*> keyToNode;
-    
-    void insertToEnd(int key, int value) {
-        if(isFull() || keyToNode.count(key)!=0) return;
-        Node *nd = new Node(key, value);
-        keyToNode[key] = nd;
-        if(!head) {
-            head = tail = nd;
-        }
-        else {
-            tail->next = nd;
-            nd->prev = tail;
-            tail = tail->next;
-        }
-    } 
-    
-    void removeHead() {
-        if(!head) return;
-        keyToNode.erase(head->key);
-        Node *temp = head;
-        if(head==tail) // only one node remain
-            head = tail = NULL;
-        else {
-            head = head->next;
-            head->prev = NULL;
-        }
-        delete temp;
-    }
 
-    void moveToEnd(int key) {
-        // key not exist, or already at the end
-        if(keyToNode.count(key)==0 || keyToNode[key]==tail) return;
-        Node *nd = keyToNode[key];
-        if(nd==head) {
-            head = head->next;
-            head->prev = NULL;
-        }
-        else {  // not head, not tail
-            nd->prev->next = nd->next;
-            nd->next->prev = nd->prev; 
-        }
-        
-        tail->next = nd;
-        nd->prev = tail;
-        nd->next = NULL;
-        tail = tail->next;
-    }
-    
-public:
+
     LRUCache(int capacity) {
         maxSize = capacity;
         head = NULL;
         tail = NULL;
         keyToNode.clear();
     }
-    
+
     int get(int key) {
         if(keyToNode.count(key)==0) return -1;
         moveToEnd(key);
-        return keyToNode[key]->val;
+        retrun keyToNode[key]->val
     }
-    
+
     void set(int key, int value) {
-        // key already exists
-        if(get(key)!=-1) {
-            keyToNode[key]->val = value;
-            return;
+        if(get(key)!=-1)
+        {
+                keyToNode[key]->val =value;
+                return;
         }
-        
-        // key not exist, insert new node
         if(isFull()) removeHead();
-        insertToEnd(key, value);
+        insertToEnd(key,value);
+
     }
-    
-    bool isFull() {
+    bool isFull()
+    {
         return keyToNode.size()>=maxSize;
     }
+
+    void insertToEnd(int key,int value)
+    {
+        if(isFull||keyToNode.count(key)!=0)
+        {
+            return;
+        }
+        Node *nd = new Node(key,value);
+        keyToNode[key] = nd;
+        if(!head)
+        {
+            head = tail =nd;
+        }
+        else
+        {
+            tail->next = nd;
+            nd ->prev = tail;
+            tail = tail->next;
+        }
+    }
+    
+    void removeHead()
+    {
+        if(!head) return;
+        keyToNode.erase(head->key);
+        Node *temp = head;
+        if(head==tail)
+            head = tail =NULL;
+        else
+        {
+            head = head->next;
+            head->prev = NULL;
+        }
+        delete temp
+    }
+    
+    void moveToEnd(int key)
+    {
+        if(keyToNode.count(key)==0||keyToNode[key]==tail) return;
+        Node *nd = keyToNode[key];
+        if(nd==head)
+        {
+            head = head->next;
+            head->prev = NULL;
+        }
+        else
+        {
+            nd->prev->next = nd->next;
+            nd ->next ->prev = nd->prev;
+        }
+        
+        tail->next = nd;
+        nd->prev = nd;
+        ne->next =NULL;
+        tail = tail->next;
+    
+    
+    }
+
 };
