6c70ac57642906d087bf9ed3e4e34c4fc5ae31fc
ndhuanhuan <ndhuanhuan@gmail.com>
2016-06-05 11:06:58 +0800

    [162. Find Peak Element][Compile Error]committed by LeetCode Extension

---
+++ 162-Find-Peak-Element/162. Find Peak Element.cpp
@@ -0,0 +1,34 @@
+
+int findPeakElement(const vector<int> &num) {
+
+    int n = num.size();
+    int low = 0;
+    int high = n - 1;
+
+    int mid = 0, v1, v2;
+
+    while ( low < high ) {
+
+        // Find the index of middle element
+        mid = low + ( high - low ) / 2;
+
+        // Compare middle element with its neighbours (if neighbours exist)
+        if ( ( mid == 0 || num[mid] > num[mid-1] ) && 
+                ( mid == n-1 ||  num[mid] > num[mid+1] )  ){
+            return mid;
+        }
+
+        // If middle element is not peak and its left neighbor is greater than it
+        // then left half must have a peak element
+        if (mid >0 && num[mid-1] > num[mid]){
+            high = mid - 1;
+        // If middle element is not peak and its right neighbor is greater than it
+        // then right half must have a peak element
+        }else{
+            low = mid + 1;
+        }
+
+    }
+
+    return low;
+} 
