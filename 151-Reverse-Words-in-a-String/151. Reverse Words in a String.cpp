77b8d4cd1817654c27ac9056c5ffe9bc46820cbc
ndhuanhuan <ndhuanhuan@gmail.com>
2016-06-06 01:03:38 +0800

    [151. Reverse Words in a String][Accepted]committed by LeetCode Extension

--- 151-Reverse-Words-in-a-String/151. Reverse Words in a String.cpp
+++ 151-Reverse-Words-in-a-String/151. Reverse Words in a String.cpp
@@ -10,14 +10,17 @@ public:
                 end = s.length();
             }
             // Reverse each word in the string.
+            cout<<end<<endl;
             reverse(s.begin() + begin, s.begin() + end);
 
             // Shift the word to avoid extra space.
+            //Moves the elements in the range [first,last) into the range beginning at result
             move(s.begin() + begin, s.begin() + end, s.begin() + len);
             len += end - begin;
+            //cout<<len<<endl;
             s[len++] = ' ';
         }
-        
+
         //remove last space
         s.resize(len ? len - 1 : 0);
     }
