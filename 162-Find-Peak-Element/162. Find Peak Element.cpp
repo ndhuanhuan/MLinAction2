20ea62f5f7d7239ff359a8855d565fc4453fa3b6
ndhuanhuan <ndhuanhuan@gmail.com>
2016-06-05 11:07:43 +0800

    [162. Find Peak Element][Accepted]committed by LeetCode Extension

--- 162-Find-Peak-Element/162. Find Peak Element.cpp
+++ 162-Find-Peak-Element/162. Find Peak Element.cpp
@@ -1,34 +1,39 @@
 
-int findPeakElement(const vector<int> &num) {
+class Solution {
+public:
 
-    int n = num.size();
-    int low = 0;
-    int high = n - 1;
+	int findPeakElement(const vector<int> &num) {
 
-    int mid = 0, v1, v2;
+		int n = num.size();
+		int low = 0;
+		int high = n - 1;
 
-    while ( low < high ) {
+		int mid = 0, v1, v2;
 
-        // Find the index of middle element
-        mid = low + ( high - low ) / 2;
+		while (low < high) {
 
-        // Compare middle element with its neighbours (if neighbours exist)
-        if ( ( mid == 0 || num[mid] > num[mid-1] ) && 
-                ( mid == n-1 ||  num[mid] > num[mid+1] )  ){
-            return mid;
-        }
+			// Find the index of middle element
+			mid = low + (high - low) / 2;
 
-        // If middle element is not peak and its left neighbor is greater than it
-        // then left half must have a peak element
-        if (mid >0 && num[mid-1] > num[mid]){
-            high = mid - 1;
-        // If middle element is not peak and its right neighbor is greater than it
-        // then right half must have a peak element
-        }else{
-            low = mid + 1;
-        }
+			// Compare middle element with its neighbours (if neighbours exist)
+			if ((mid == 0 || num[mid] > num[mid - 1]) &&
+				(mid == n - 1 || num[mid] > num[mid + 1])) {
+				return mid;
+			}
 
-    }
+			// If middle element is not peak and its left neighbor is greater than it
+			// then left half must have a peak element
+			if (mid >0 && num[mid - 1] > num[mid]) {
+				high = mid - 1;
+				// If middle element is not peak and its right neighbor is greater than it
+				// then right half must have a peak element
+			}
+			else {
+				low = mid + 1;
+			}
 
-    return low;
-} 
+		}
+
+		return low;
+	}
+};
