c9eebcf7ce32b631c47aa736a9df7d893d1302d9
ndhuanhuan <ndhuanhuan@gmail.com>
2016-06-09 11:56:23 +0800

    [71. Simplify Path][Compile Error]committed by LeetCode Extension

---
+++ 71-Simplify-Path/solution.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        string ret, curDir;
+        vector<string> allDir;
+        
+        path.push_back('/');
+        for(int i=0; i<path.size(); i++) {
+            if(path[i]=='/') {
+                if(curDir.empty()) {
+                    continue;
+                }
+                else if(curDir==".") {
+                    curDir.clear();
+                }
+                else if(curDir=="..") {
+                    if(!allDir.empty())
+                        allDir.pop_back();
+                    curDir.clear();
+                }
+                else {
+                    allDir.push_back(curDir);
+                    curDir.clear();
+                }
+            }
+            else {
+                curDir.push_back(path[i]);
+            }
+        }
+        
+        
+        for(int i=0; i<allDir.size(); i++) 
+            ret.append("/"+allDir[i]);
+        if(ret.empty()) ret = "/";
+        return ret;
+    }
