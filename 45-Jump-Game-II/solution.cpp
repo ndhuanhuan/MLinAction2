d8b4b7592f0f1ad2b0a8cf9d6cd244ece23f8d43
ndhuanhuan <ndhuanhuan@gmail.com>
2016-06-10 13:00:12 +0800

    [45. Jump Game II][Compile Error]committed by LeetCode Extension

---
+++ 45-Jump-Game-II/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        int curMax = 0, njumps = 0, i = 0;
+        while(curMax<n-1) {
+            int lastMax = curMax;
+            for(; i<=lastMax; i++) 
+                curMax = max(curMax,i+A[i]);
+            njumps++;
+            if(lastMax == curMax) return -1;
+        }
+        return njumps;  
+    }
+};
