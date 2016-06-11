fff5541f13d20492eae7a35053ae321037ed6f69
ndhuanhuan <ndhuanhuan@gmail.com>
2016-06-12 00:57:27 +0800

    [47. Permutations II][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 47-Permutations-II/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+   int n = nums.size();
+		vector<vector<int>> result;
+		vector<int> path;
+		dfs(nums, path, result);
+		return result;
+	}
+
+	void dfs(const vector<int>& nums, vector<int> &path, vector<vector<int> > &result)
+	{
+		if (path.size() == nums.size()) 
+		{
+			result.push_back(path);
+			return;
+		}
+		for (auto i : nums) 
+		{
+			//auto pos = find(path.begin(), path.end(), i);
+			
+				path.push_back(i);
+				dfs(nums, path, result);
+				path.pop_back();
+		
+		}
+	
+	}
+};
