04d8a81148893172c0ba130213abd860a5aa757d
ndhuanhuan <ndhuanhuan@gmail.com>
2016-07-05 02:03:47 +0800

    [149. Max Points on a Line][Accepted]committed by LeetCode Extension

---
+++ 149-Max-Points-on-a-Line/solution.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a point.
+ * struct Point {
+ *     int x;
+ *     int y;
+ *     Point() : x(0), y(0) {}
+ *     Point(int a, int b) : x(a), y(b) {}
+ * };
+ */
+class Solution {
+public:
+    int maxPoints(vector<Point> &points) {
+        int maxPts = 0;
+        for(int i=0; i<points.size(); i++) {
+            int nMax = 0, nSame = 0, nInf = 0;
+            unordered_map<float,int> comSlopes;
+            
+            for(int j=i+1; j<points.size(); j++) {
+                if(points[j].x==points[i].x) {
+                    if(points[j].y==points[i].y)
+                        nSame++;
+                    else
+                        nInf++;
+                    continue;
+                }
+                float slope = (float)(points[j].y-points[i].y)/(float)(points[j].x-points[i].x);
+                comSlopes[slope]++;
+                nMax = max(nMax, comSlopes[slope]);
+            }
+            
+            nMax = max(nMax, nInf)+nSame+1;
+            maxPts = max(maxPts,nMax);
+        }
+        return maxPts;
+    }
+};
