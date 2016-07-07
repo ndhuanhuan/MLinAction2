f2e3635bd300e7bbfe6407ee2f30563a3355d57f
ndhuanhuan <ndhuanhuan@gmail.com>
2016-07-07 11:43:53 +0800

    [323. Number of Connected Components in an Undirected Graph][Accepted]committed by LeetCode Extension

---
+++ 323-Number-of-Connected-Components-in-an-Undirected-Graph/solution.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int countComponents(int n, vector<pair<int, int> >& edges) {
+        root = vector<int>(n, -1);
+        int size = edges.size();
+        int count = n;
+        for(int i = 0; i < size; ++i)
+        {
+            int u = edges[i].first;
+            int v = edges[i].second;
+            int uroot = findRoot(u);
+            int vroot = findRoot(v);
+            if(uroot != vroot)
+            {
+                --count;
+                unionSet(uroot, u, vroot, v);
+            }
+        }
+        
+        return count;
+    }
+private:
+    void unionSet(int uroot, int u, int vroot, int v)
+    {
+        root[uroot] = vroot;
+    }
+    int findRoot(int u)
+    {
+        if(root[u] == -1) return u;
+        root[u] = findRoot(root[u]);
+        return root[u];
+    }
+private:
+    vector<int> root;
+    
+};
