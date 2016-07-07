c34838f984191b474bd15f8b927a4f823c728245
ndhuanhuan <ndhuanhuan@gmail.com>
2016-07-07 11:26:21 +0800

    [295. Find Median from Data Stream][Accepted]committed by LeetCode Extension

---
+++ 295-Find-Median-from-Data-Stream/solution.cpp
@@ -0,0 +1,32 @@
+
+class MedianFinder {
+	priority_queue<int> small, large;
+public:
+	// Adds a number into the data structure.
+	void addNum(int num) {
+		if (!large.empty() && -large.top() < num)
+			large.push(-num);
+		else
+			small.push(num);
+ 
+		if (small.size() - large.size() == 2) {
+			large.push(-small.top());
+			small.pop();
+		}
+		else if (small.size() - large.size() == -2) {
+			small.push(-large.top());
+			large.pop();
+		}
+	}
+ 
+	// Returns the median of current data stream
+	double findMedian() {
+		if (small.size() > large.size()) return small.top();
+		else if (small.size() < large.size()) return -large.top();
+		return (small.top() - large.top()) / 2.0;
+	}
+};
+// Your MedianFinder object will be instantiated and called as such:
+// MedianFinder mf;
+// mf.addNum(1);
+// mf.findMedian();
