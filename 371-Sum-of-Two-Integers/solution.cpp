825bad3f6591c577859fd8c4461eb4f5bbe62606
ndhuanhuan <ndhuanhuan@gmail.com>
2016-07-14 11:39:13 +0800

    [371. Sum of Two Integers][Runtime Error]committed by LeetCode Extension

--- 371-Sum-of-Two-Integers/solution.cpp
+++ 371-Sum-of-Two-Integers/solution.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        int cur = a&b;
-        int carry = (a^b)<<1;
+        int cur = a^b;
+        int carry = (a&b)<<1;
         return getSum(cur,carry);
     }
 };
