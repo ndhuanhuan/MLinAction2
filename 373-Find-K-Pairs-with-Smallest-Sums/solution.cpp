ccb1771bb1e097130731883df7495693773f6b11
ndhuanhuan <ndhuanhuan@gmail.com>
2016-07-15 11:37:32 +0800

    [373. Find K Pairs with Smallest Sums][Compile Error]committed by LeetCode Extension

--- 373-Find-K-Pairs-with-Smallest-Sums/solution.cpp
+++ 373-Find-K-Pairs-with-Smallest-Sums/solution.cpp
@@ -3,13 +3,13 @@ public:
     vector<pair<int, int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
         vector<pair<int,int>> res;
 		priority_queue<pair<int,int>, vector<pair<int,int>>, cmp> q;
-		for(int i=0 ; i<min((int)num1.size,k); ++i )
+		for(int i=0 ; i<min((int)nums1.size,k); ++i )
 		{
-			for(int j;j < min((int)num2.size,k);++j)
+			for(int j;j < min((int)nums2.size,k);++j)
 			{
 				if(q.size()<k)
 				{
-					q.push({num1[i],num2[j]});
+					q.push({nums1[i],nums2[j]});
 				}
 				else if (nums1[i] + nums2[j] < q.top().first + q.top().second) 
 				{
