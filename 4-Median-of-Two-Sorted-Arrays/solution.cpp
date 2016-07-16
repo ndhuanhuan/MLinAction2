190105211e60f0ff74092c5ee18086bf57cee547
ndhuanhuan <ndhuanhuan@gmail.com>
2016-07-17 06:33:36 +0800

    [4. Median of Two Sorted Arrays][Compile Error]committed by LeetCode Extension

--- 4-Median-of-Two-Sorted-Arrays/solution.cpp
+++ 4-Median-of-Two-Sorted-Arrays/solution.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
-   double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) 
-		{
-			const int m = nums1.size();
-			const int n = nums2.size();
-			int total = m + n;
-			if (total & 0x1)
-			{
-				return find_kth(nums1.begin(),m,nums2.begin(),n,total/2+1);
-			}
-			else
-			{
-				return (find_kth(nums1.begin(),m,nums2.begin(),n,total/2+1)+find_kth(nums1.begin(),m,nums2.begin(),n,total/2))/2.0;
-			}
-		}
-
-		static int find_kth(vector<int>::const_iterator A, int m, vector<int>::const_iterator B, int n, int k) 
-		{
-			if (m > n) 
-			{
-				return find_kth(B, n, A, m, k);
-			}
-			if (m == 0) return *(B + k - 1);
-			if (k == 1) return min(*A, *B);
-			int ia = min(k / 2, m);
-			int ib = k - ia;
-
-			if (*(A + ia - 1) < *(B + ib - 1)) 
-			{
-				return find_kth(A + ia, m - ia, B, n, k - ia);
-			}
-			else if (*(A + ia - 1) > *(B + ib - 1))
-				return find_kth(A, m, B + ib, n - ib, k - ib);
-			else
-				return A[ia - 1];
-		}
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) 
+    {
+        const int n1 = nums1.size();
+        const int n2 = nums2.size();
+        int total =n1+n2;
+        if(total&0x1)
+        {
+            return find_kth(nums1,n1,nums2,n2, total/2+1);
+        }
+        else
+        {
+            return (find_kth(nums1,n1,nums2,n2, total/2+1)+find_kth(nums1,n1,nums2,n2, total/2))/2.0;
+        }
+    }
+    
+    int find_kth(vector<int>::const_iterator A,int n1, vector<int>::const_iterator B, int n2, int k)
+    {
+        if(n1>n2)
+        {
+            return find_kth(B,n2, A, n1,k);
+        }
+        if(n1==0) return *(B+k-1);
+        int ia = min(n1,k/2);
+        int ib = k-ia;
+        if(*(A+ia-1)<*(B+ib-1))
+        {
+            return find_kth(A+ia,n1-ia,B,n2,k-ia);
+        }
+        else if(*(A+ia-1)<*(B+ib-1))
+        {
+            return find_kth(A, m, B + ib, n - ib, k - ib);
+        }
+        else
+            return A[ia-1]; 
+    }
 };
