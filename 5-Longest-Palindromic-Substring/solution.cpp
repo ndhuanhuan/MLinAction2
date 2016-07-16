5bb84ac59d2eb8b8b48b710a5b60eef0b0abee38
ndhuanhuan <ndhuanhuan@gmail.com>
2016-07-17 07:30:50 +0800

    [5. Longest Palindromic Substring][Compile Error]committed by LeetCode Extension

--- 5-Longest-Palindromic-Substring/solution.cpp
+++ 5-Longest-Palindromic-Substring/solution.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-	string longestPalindrome(string s) {
-		if (s.size() <= 1) return s;
-		int start = 0, maxLen = 1;
-		const int n = s.length();
-		bool isPal[n][n] = { false };
-		for (int i = n - 1; i >= 0; i--) 
-		{
-			for (int j = i; j < n; j++) 
-			{
-				if ((i + 1>j - 1 || isPal[i + 1][j - 1]) && s[i] == s[j]) 
-				{
-					isPal[i][j] = true;
-					if (j - i + 1 > maxLen) 
-					{
-						maxLen = j - i + 1;
-						start = i;
-					}
-
-				}
-			}
-		}
-		return s.substr(start, maxLen);
-
-	}
+    string longestPalindrome(string s) {
+        if(s.length()<=1) return s;
+        int start=0, maxLen=1;
+        const int n = s.length();
+        bool isPal[n][n] = {false};
+        for(int i=n-1; i>=0; i--)
+        {
+            for(int j=i ; j<n; j++)
+            {
+                if((i+>j-1 || isPal[i+1][j-1])&&s[i]==s[j])
+                {
+                    isPal[i][j] = true;
+                    if(j-i+1>maxLen)
+                    {
+                        maxLen = j-i+1;
+                        start = i;
+                    }
+                }
+            }
+        }
+        return s.substr(start, maxLen);
+    }
 };
