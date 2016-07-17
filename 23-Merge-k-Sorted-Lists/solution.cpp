8dff1803dbd7b9e544692c95534a147d7432c57e
ndhuanhuan <ndhuanhuan@gmail.com>
2016-07-18 03:03:33 +0800

    [23. Merge k Sorted Lists][Compile Error]committed by LeetCode Extension

--- 23-Merge-k-Sorted-Lists/solution.cpp
+++ 23-Merge-k-Sorted-Lists/solution.cpp
@@ -8,34 +8,37 @@
  */
 class Solution {
 public:
-	ListNode *mergeKLists(vector<ListNode *> &lists) {
-		if (lists.empty()) return NULL;
-		int end = lists.size() - 1;
-		while (end>0) {
-			int begin = 0;
-			while (begin<end) {
-				lists[begin] = merge2Lists(lists[begin], lists[end]);
-				begin++;
-				end--;
-			}
-		}
-		return lists[0];
-	}
-
-	ListNode* merge2Lists(ListNode *h1, ListNode *h2) {
-		ListNode *dummy = new ListNode(0), *tail = dummy;
-		while (h1 && h2) {
-			if (h1->val <= h2->val) {
-				tail->next = h1;
-				h1 = h1->next;
-			}
-			else {
-				tail->next = h2;
-				h2 = h2->next;
-			}
+    stuct compNode
+    {
+        bool operator()(ListNode* l1, ListNode* l2) const
+        {
+            if(l1->val>=l2)
+            {
+                return true;
+            }
+            else
+            {
+                return false
+            }
+        }
+    };
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, compNode> pq;
+        ListNode dummy(-1);
+        ListNode *tail = dummy;
+        for(int i=0;i<lists.size(); i++)
+        {
+            if(lists[i])
+            {
+                pq.push(lists[i]);
+            }
+        }
+        while (!pq.empty()) {
+			tail->next = pq.top();
 			tail = tail->next;
+			pq.pop();
+			if (tail->next) pq.push(tail->next);
 		}
-		tail->next = h1 ? h1 : h2;
 		return dummy->next;
-	}
+    }
 };
