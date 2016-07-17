419a628a867a2fce844a471a5465882a7a71eac9
ndhuanhuan <ndhuanhuan@gmail.com>
2016-07-18 03:04:38 +0800

    [23. Merge k Sorted Lists][Compile Error]committed by LeetCode Extension

--- 23-Merge-k-Sorted-Lists/solution.cpp
+++ 23-Merge-k-Sorted-Lists/solution.cpp
@@ -25,7 +25,7 @@ public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         priority_queue<ListNode*, vector<ListNode*>, compNode> pq;
         ListNode dummy(-1);
-        ListNode *tail = dummy;
+        ListNode *tail = &dummy;
         for(int i=0;i<lists.size(); i++)
         {
             if(lists[i])
