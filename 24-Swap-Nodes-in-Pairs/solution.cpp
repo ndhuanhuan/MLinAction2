7a965c40b44eb96f9e39fbce06cf70d93491111f
ndhuanhuan <ndhuanhuan@gmail.com>
2016-07-18 03:18:43 +0800

    [24. Swap Nodes in Pairs][Compile Error]committed by LeetCode Extension

--- 24-Swap-Nodes-in-Pairs/solution.cpp
+++ 24-Swap-Nodes-in-Pairs/solution.cpp
@@ -7,18 +7,18 @@
  * };
  */
 class Solution {
-    public:
-        ListNode *swapPairs(ListNode *head) {
-            if (head == nullptr || head->next == nullptr) return head;
-            ListNode dummy(-1);
-            dummy.next = head;
-            for(ListNode *prev = &dummy, *cur = prev->next, *next = cur->next;
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode dummy(-1);
+        head->next = &dummy;
+        for(ListNode *prev=dummy, *cur = prev->next, *next = cur->next;
             next;
-            prev = cur, cur = cur->next, next = cur ? cur->next: nullptr) {
-            prev->next = next;
-            cur->next = next->next;
-            next->next = cur;
-        }
-        return dummy.next;
+            prev = cur, cur = cur->next, next = cur? cur->next: nullptr
+        )
+        prev->next =next;
+        cur->next = next->next;
+        next ->next =cur;
+        
     }
+    return dummy.next;
 };
