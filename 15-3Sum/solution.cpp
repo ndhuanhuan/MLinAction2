957060d1b81bf2bccae8a4c7689ce7e981fe3889
ndhuanhuan <ndhuanhuan@gmail.com>
2016-07-17 23:05:34 +0800

    [15. 3Sum][Compile Error]committed by LeetCode Extension

--- 15-3Sum/solution.cpp
+++ 15-3Sum/solution.cpp
@@ -4,36 +4,37 @@ public:
         vector<vector<int>> result;
         if(nums.size()<3) return result;
         sort(nums.begin(),nums.end());
-        const int target = 0;
-
+        const int target=0;
+        
         auto last = nums.end();
-        for(auto i=nums.begin();i<last-2;i++)
+        for(auto i = nums.begin();i<last-2;i++)
         {
-            auto j=i+1;
+            auto j = i+1;
             if(i>nums.begin()&&*i==*(i-1)) continue;
-            auto k=last-1;
+            auto k = last-1;
             while(j<k)
             {
-                if (*i + *j + *k < target)
+                if(*i+*j+*k<target)
                 {
                     ++j;
-                    while(*j == *(j - 1) && j < k) ++j;
+                    while(*j==*(j-1)&&j<k) ++j;
                 }
-                else if (*i + *j + *k > target)
+                else if(*i+*j+*k>target)
                 {
                     --k;
-                    while(*k == *(k + 1) && j < k) --k;
+                    while(*k==*(k+1)&&j<k) --k;
                 }
                 else
                 {
-                    result.push_back({ *i, *j, *k });
+                    result.push({*i,*j,*k});
                     ++j;
                     --k;
-                    while(*j == *(j - 1) && *k == *(k + 1) && j < k) ++j;
+                    while(*j==*(j-1)&&j<k) ++j;
+                    while(*k==*(k+1)&&j<k) --k;
                 }
-
             }
         }
         return result;
+        
     }
 };
