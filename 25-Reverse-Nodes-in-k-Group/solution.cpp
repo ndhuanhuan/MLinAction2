1643b0eebf3abe524c66b2c4933d4576dea18df6
ndhuanhuan <ndhuanhuan@gmail.com>
2016-07-18 03:48:54 +0800

    [25. Reverse Nodes in k-Group][Compile Error]committed by LeetCode Extension

--- 25-Reverse-Nodes-in-k-Group/solution.cpp
+++ 25-Reverse-Nodes-in-k-Group/solution.cpp
@@ -7,31 +7,34 @@
  * };
  */
 class Solution {
-    public:
-        ListNode *reverseKGroup(ListNode *head, int k) 
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if(head == nullptr||head->next==nullptr||k<2) return head;
+        ListNode dummy(-1);
+        dummy.next = head;
+        for(ListNode *prev=&dummy, *end = head; end; end = prev->next)
         {
-            if (head == nullptr || head->next == nullptr || k < 2) return head;
-            ListNode dummy(-1);
-            dummy.next = head;
-            for(ListNode *prev = &dummy, *end = head; end; end = prev->next) {
-                for (int i = 1; i < k && end; i++)
-                end = end->next;
-                if (end == nullptr) break;
-                prev = reverse(prev, prev->next, end);
-            }
-            return dummy.next;
-        }
-
-            ListNode* reverse(ListNode *prev, ListNode *begin, ListNode *end) 
+            for (int i=0; i<k&&end; i++)
             {
-                ListNode *end_next = end->next;
-                for (ListNode *p = begin, *cur = p->next, *next = cur->next;
-                    cur != end_next;
-                    p = cur, cur = next, next = next ? next->next : nullptr) {
-                    cur->next = p;
-                }
-                begin->next = end_next;
-                prev->next = end;
-                return begin;
+                end = end->next;
             }
+            if(end == nullptr) break;
+            prev = reverse(prev,prev->next,end);
+        }
+        
+        ListNode* reverse(ListNode *prev, ListNode *begin, ListNode *end)
+        {
+            ListNode *end_next = end->next;
+            for(ListNode *p = begin, cur = p->next, next = cur->next;
+                cur! = end_next;
+                p = cur; cur =next; next = next? next->next: nullptr
+            )
+            {
+              cur ->next = p   
+            }
+            begin->next = end_next;
+            prev->next = end;
+            return begin;
+        }
+    }
 };
