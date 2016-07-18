565423bbd25056f6f4b0c64298feb3f866e6ed6a
ndhuanhuan <ndhuanhuan@gmail.com>
2016-07-18 10:48:41 +0800

    [46. Permutations][Accepted]committed by LeetCode Extension

--- 46-Permutations/solution.cpp
+++ 46-Permutations/solution.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-	vector<vector<int>> permute(vector<int>& nums) {
-		int n = nums.size();
-		vector<vector<int>> result;
-		vector<int> path;
-		dfs(nums, path, result);
-		return result;
-	}
-
-	void dfs(const vector<int>& nums, vector<int> &path, vector<vector<int> > &result)
-	{
-		if (path.size() == nums.size()) 
-		{
-			result.push_back(path);
-			return;
-		}
-		for (auto i : nums) 
-		{
-			auto pos = find(path.begin(), path.end(), i);
-			if (pos == path.end()) 
-			{
-				path.push_back(i);
-				dfs(nums, path, result);
-				path.pop_back();
-			}
-		}
-	
-	}
-
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> result;
+        vector<int> path;
+        dfs(nums,path,result);
+        return result;
+    }
+    
+    void dfs(const vector<int>& nums, vector<int> &path, vector<vector<int>> &result)
+    {
+        if(path.size()==nums.size())
+        {
+            result.push_back(path);
+            return;
+        }
+        for(auto i: nums)
+        {
+            auto pos = find(path.begin(),path.end(),i);
+            if(pos == path.end())
+            {
+                path.push_back(i);
+                dfs(nums,path,result);
+                path.pop_back();
+            }
+        }
+        
+    }
 };
