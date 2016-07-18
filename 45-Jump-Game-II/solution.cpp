c5c2725a68e484a11b8d4a77d2dc2676ca8f9315
ndhuanhuan <ndhuanhuan@gmail.com>
2016-07-18 08:39:00 +0800

    [45. Jump Game II][Accepted]committed by LeetCode Extension

--- 45-Jump-Game-II/solution.cpp
+++ 45-Jump-Game-II/solution.cpp
@@ -2,14 +2,17 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n = nums.size();
-        int curMax = 0, njumps = 0, i = 0;
-        while(curMax<n-1) {
+        int curMax = 0;
+        int n_jumps = 0;
+        int i=0;
+        while(curMax<n-1)
+        {
             int lastMax = curMax;
-            for(; i<=lastMax; i++) 
+            for(;i<=lastMax;i++)
                 curMax = max(curMax,i+nums[i]);
-            njumps++;
-            if(lastMax == curMax) return -1;
+            n_jumps++;
+            if(lastMax==curMax) return -1;
         }
-        return njumps;  
+        return n_jumps;
     }
 };
