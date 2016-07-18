d3e5a6b6759f5a1ba2eb0a9d7736d7a6dc5603c8
ndhuanhuan <ndhuanhuan@gmail.com>
2016-07-18 08:19:52 +0800

    [40. Combination Sum II][Compile Error]committed by LeetCode Extension

--- 40-Combination-Sum-II/solution.cpp
+++ 40-Combination-Sum-II/solution.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-	vector<vector<int> > combinationSum2(vector<int> &nums, int target) {
-		sort(nums.begin(), nums.end());
-		vector<vector<int> > result; 
-		vector<int> path; 
-		dfs(nums, path, result, target, 0);
-		return result;
-	}
-private:
-	void dfs(vector<int>& nums, vector<int>& path, vector<vector<int> > &result,
-		int gap, int start) {
-		if (gap == 0) { 
-			result.push_back(path);
-			return;
-		}
-		int prev = -1;
-		for (size_t i = start; i < nums.size(); i++) { 
-			if (prev == nums[i]) continue;
-			if (gap < nums[i]) return; 
-			prev = nums[i];
-			path.push_back(nums[i]); 
-			dfs(nums, path, result, gap - nums[i], i+1);
-			path.pop_back(); 
-		}
-	}
+    vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
+        vector<vector<int>> result;
+        sort(nums.begin(),nums.end());
+        vector<int> path;
+        dfs(nums,path,result,target,0);
+        return result;
+    }
+    
+    void dfs(vector<int>& nums, vector<int>& path, vector<vector<int>>& result, int gap, int start)
+    {
+        if(gap==0)
+        {
+            result.push_back(path);
+            return;
+        }
+        
+        int prev=-1;
+        for(int i=start; i<nums.size(); i++)
+        {
+            if(prev==nums[i]) continue;
+            if(gap<nums[i]) return;
+            prev = nums[i];
+            path.push_back(nums[i]);
+            dfs(nums,path,result,target-nums[i],i+1);
+            path.pop_back();
+        }
+    }
 };
