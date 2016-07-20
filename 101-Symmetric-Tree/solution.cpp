a19e1759cf66340cfdde33b086f220b1db595c48
ndhuanhuan <ndhuanhuan@gmail.com>
2016-07-20 09:22:26 +0800

    [101. Symmetric Tree][Compile Error]committed by LeetCode Extension

--- 101-Symmetric-Tree/solution.cpp
+++ 101-Symmetric-Tree/solution.cpp
@@ -14,12 +14,15 @@ public:
         stack<TreeNode*> s;
         s.push(root->left);
         s.push(root->right);
-        while (!s.empty ()) {
-            auto p = s.top (); s.pop();
-            auto q = s.top (); s.pop();
-            if (!p && !q) continue;
-            if (!p || !q) return false;
-            if (p->val != q->val) return false;
+        while(s)
+        {
+            int p = s.top();
+            s.pop();
+            int q = s.top();
+            s.pop();
+            if(!p && !q) continue;
+            if(!p||!q) return false;
+            if(p->val!=q->val) return false;
             s.push(p->left);
             s.push(q->right);
             s.push(p->right);
